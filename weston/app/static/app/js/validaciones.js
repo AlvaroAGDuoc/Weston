8981ecb1c3069b3bae45fe7ac38ffd32d9ffb394
ReneRojasR <101284294+ReneRojasR@users.noreply.github.com>
2022-05-23 15:15:59 -0400

    Solución (Registro, Producto)

--- weston/app/static/app/js/validaciones.js
+++ weston/app/static/app/js/validaciones.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	/* Validaciones registro*/
 	$('#form_reg').submit(function (e) {
-		e.preventDefault();
+		
 		var nombre = $('#nombre_reg').val();
 		var clave = $('#pass_reg').val();
 		var clave2 = $('#pass_reg2').val();
@@ -17,21 +17,28 @@ $(document).ready(function () {
 
 		//VALIDACIONES NOMBRE
 		if (nombre.trim().length < 3 || nombre.trim().length > 25) {
+			
 			mensaje += 'El nombre no es valido. <br>';
 			entrar = true;
+			e.preventDefault();
+
 		}
 
 		if(/[0-9]/.test(nombre)){
+			
             mensaje += "El nombre no puede tener digitos. <br>"
             entrar = true;
+			e.preventDefault();
         }
 
 		//NO ESTOY MUY SEGURO DE ESTA VALIDACION PERO POR LO QUE EH PROBADO SIRVE
 		if(nombre.charAt(0) === (nombre.charAt(0).toUpperCase())){
 
 		}else{
+			
 			mensaje += "El nombre debe tener la primera letra Mayuscula. <br>"
             entrar = true;
+			e.preventDefault();
 		}
 		//FIN VALIDACIONES NOMBRE
 
@@ -41,21 +48,25 @@ $(document).ready(function () {
 		if (clave.trim().length < 6) {
 			mensaje += 'La clave debe tener un largo minimo de 6. <br>';
 			entrar = true;
+			e.preventDefault();
 		}
 
 		if (clave != clave2) {
-            mensaje += "Las claves no coinciden. <br>"
+			mensaje += "Las claves no coinciden. <br>"
             entrar = true;
+			e.preventDefault();
+			
+			if(!/[A-Z]/.test(clave)){
         }
-
-		if(!/[A-Z]/.test(clave)){
-            mensaje += "La clave no contiene una mayuscula. <br>"
-            entrar = true;
+		mensaje += "La clave no contiene una mayuscula. <br>"
+		entrar = true;
+		e.preventDefault();
         }
 
 		if(!/[0-9]/.test(clave)){
-            mensaje += "La clave no contiene un digito. <br>"
+			mensaje += "La clave no contiene un digito. <br>"
             entrar = true;
+			e.preventDefault();
         }
 		//FIN VALIDACIONES CLAVE
 
@@ -65,6 +76,7 @@ $(document).ready(function () {
 		if (!regexEmail.test(correo)) {
 			mensaje += 'El email no es valido. <br>';
 			entrar = true;
+			e.preventDefault();
 		}
 		//FIN VALIDACIONES EMAIL
 
@@ -74,11 +86,13 @@ $(document).ready(function () {
 		if (dir.trim().length < 8 || dir.trim().length > 30) {
 			mensaje += 'El pasaje debe tener un largo minimo de 8 y maximo de 30.<br>';
 			entrar = true;
+			e.preventDefault();
 		}
 
 		if(!/[a-z]/.test(dir) && (!/[A-Z]/.test(dir))){
-            mensaje += "La direccion no contiene caracteres. <br>"
+			mensaje += "La direccion no contiene caracteres. <br>"
             entrar = true;
+            e.preventDefault();
         }
 		//FIN VALIDACIONES DIRECCION
 
@@ -90,11 +104,13 @@ $(document).ready(function () {
 		if (celu.trim().length != 9) {
 			mensaje += 'El celular debe tener un largo de 9. <br>';
 			entrar = true;
+			e.preventDefault();
 		}
 		//FIN VALIDACIONES CELULAR
 
 		//-----------------------//
 		if (entrar) {
+			
 			$('#warnings').html(mensaje);
 		} else {
 			$('#warnings').html('Registrado correctamente');
@@ -259,7 +275,7 @@ $(document).ready(function () {
 
 	/* Validaciones agregar producto */
 	$('#form_agregar').submit(function (e) {
-		e.preventDefault();
+		
 		var nombre = $('#nombre_prod').val();
 		var precio = $('#precio_prod').val();
 		var descripcion = $('#desc_prod').val();
--- weston/weston/settings.py
+++ weston/weston/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'weston.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl',
+        'NAME': '127.0.0.1:1521/xe',
         'USER': 'westonadm',
         'PASSWORD': 'admin123',
         'TEST': {
