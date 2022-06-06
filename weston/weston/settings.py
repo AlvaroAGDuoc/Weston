b661c2dfe4c0ec3ed8d116a2f5b7b4f3c3bdfa02
ReneRojasR <101284294+ReneRojasR@users.noreply.github.com>
2022-06-06 14:28:10 -0400

    pequeño error modProducto

--- weston/weston/settings.py
+++ weston/weston/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'weston.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl',
+        'NAME': '127.0.0.1:1521/xe',
         'USER': 'adminweston',
         'PASSWORD': 'admin123',
         'TEST': {
